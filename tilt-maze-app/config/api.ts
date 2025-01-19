f8b2abb5886907a4f73a639da22c242eef6e4bc9
rishiraj-dblue <rishiraj.salam@dblue.it>
2025-01-19 02:11:58 +0100

    axios base url udpate

--- tilt-maze-app/config/api.ts
+++ tilt-maze-app/config/api.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 // Replace with your computer's IP address and backend port
 const server = "164.90.227.121";
 const local = "192.168.1.59"
-const API_URL = `http://${server}:3001/api/v1`; 
+const API_URL = `http://${server}/api/v1`; 
 
 const api = axios.create({
   baseURL: API_URL,
